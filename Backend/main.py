181cafa7f37c5f5290bf4e942cb1794570680500
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-07-03 16:51:56 +0800

    后端：图片上传api

--- Backend/main.py
+++ Backend/main.py
@@ -7,95 +7,87 @@ from fastapi.responses import HTMLResponse,FileResponse
 
 import uuid # 生成唯一标识符
 
+URL = "http://127.0.0.1:8000/"
+
 app = FastAPI()
+
+class ImageType(str, Enum):
+    jpg = "jpg"
+    png = "png"
+    jpeg = "jpeg"
+    gif = "gif"
+
 
 class Image(BaseModel):
     UID: str
-    name: str
     url: str
     path: str
 
-
+# 图片列表 python 中的列表存储 图片对象（字典）
 IMAGELIST = [
     {
-    "UID": "eaac40f5-18e9-11ee-85f0-782b46e7b138",
-    "name": "test.jpg",
-    "url": "http://10.18.15.96:8000/getimg/?UID=eaac40f5-18e9-11ee-85f0-782b46e7b138",
-    "path": "E:\\IAP\\Backend\\app\\tmp\\eaac40f5-18e9-11ee-85f0-782b46e7b138.jpg"
+        "UID": "79411196-f0c2-4a56-9797-ea0c406df279",
+        "url": "http://127.0.0.1:8000/getimg/79411196-f0c2-4a56-9797-ea0c406df279",
+        "path": "./tmp/79411196-f0c2-4a56-9797-ea0c406df279.jpg"
+    },
+    {
+        "UID": "437fb194-fb16-4e8d-b263-1fc1d1e648ab",
+        "url": "http://127.0.0.1:8000/getimg/437fb194-fb16-4e8d-b263-1fc1d1e648ab",
+        "path": "./tmp/437fb194-fb16-4e8d-b263-1fc1d1e648ab.jpg"
     }
 ]
 
-# 获取当前网址
-def get_host():
-    import socket
-    hostname = socket.gethostname()
-    ip = socket.gethostbyname(hostname)
-    return ip
-
-
-
-# 接受图片 生成唯一标识符 保存到本地
-# 并保存到本地 E:\IAP\Backend\app\tmp
+# 上传图片
 @app.post("/uploadimg/")
-async def create_upload_files(file: UploadFile = File(...)):
-    UID = str(uuid.uuid1())
-    contents = await file.read()
-    with open(f"E:\\IAP\\Backend\\app\\tmp\\{UID}.jpg", "wb") as f:
-        f.write(contents)
-    url = f"http://{get_host()}:8000/getimg/?UID={UID}"
-    path = f"E:\\IAP\\Backend\\app\\tmp\\{UID}.jpg"
-    img = Image(UID=UID, name=file.filename, url=url, path=path)
+async def create_upload_img(file: UploadFile = File(...)):
+    # 生成唯一标识符
+    uid = uuid.uuid4()
+    # 获取文件名
+    filename = file.filename
+    # 获取文件后缀
+    suffix = filename.split(".")[-1]
+    # 拼接文件名
+    filename = str(uid) + "." + suffix
+    # 拼接文件路径
+    filepath = "./tmp/" + filename
+    # 写入文件
+    with open(filepath, "wb") as f:
+        f.write(file.file.read())
+        f.close()
+    # 拼接图片 URL
+    url = URL + "getimg/" + str(uid)
+    # 创建图片对象
+    img = Image(UID=str(uid), url=url, path=filepath)
+    # 添加到图片列表
     IMAGELIST.append(img)
+    # 返回图片对象
     return img
 
-# 根据唯一标识符获取图片
-@app.get("/getimg/")
-async def getimg(UID: str):
+
+# 根据 UID 查询图片 并返回图片  
+@app.get("/getimg/{UID}")
+async def get_img(UID: str = Path(..., title="UID", description="图片唯一标识符")):
     for img in IMAGELIST:
         if img["UID"] == UID:
             return FileResponse(img["path"])
-    raise HTTPException(status_code=404, detail="Item not found")
+    raise HTTPException(status_code=404, detail="图片不存在")
+
+# 获取图片列表
+@app.get("/getimglist/")
+async def get_img_list():
+    return IMAGELIST
 
 
+# http://127.0.0.1:8000/getimglist
 
 @app.get("/")
 async def main():
     content = """
-<body>
-<form action="/uploadimg/" enctype="multipart/form-data" method="post">
-<input name="file" type="file" multiple>
-<input type="submit">
-</form>
-</body>
+    <body>
+    <form action="/uploadimg/" enctype="multipart/form-data" method="post">
+    <input name="file" type="file" multiple>
+    <input type="submit">
+    </form>
+    </body>
     """
     return HTMLResponse(content=content)
-
-
-
-
-# @app.post("/files/")
-# async def create_files(files: list[bytes] = File()):
-#     return {"file_sizes": [len(file) for file in files]}
-
-
-# @app.post("/uploadfiles/")
-# async def create_upload_files(files: list[UploadFile]):
-
-#     return {"filenames": [file.filename for file in files]}
-
-
-# @app.get("/")
-# async def main():
-#     content = """
-# <body>
-# <form action="/files/" enctype="multipart/form-data" method="post">
-# <input name="files" type="file" multiple>
-# <input type="submit">
-# </form>
-# <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-# <input name="files" type="file" multiple>
-# <input type="submit">
-# </form>
-# </body>
-#     """
-#     return HTMLResponse(content=content)

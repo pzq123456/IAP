237f2dc1b56a29e54614cc4b1cbd7ddd06892550
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-07-05 23:46:29 +0800

    前端

--- IAP/src/App.vue
+++ IAP/src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <el-config-provider namespace="ep">
-    <BaseHeader />
-    <div class="flex main-container">
-      <BaseSide />
-      <div w="full" py="4">
-        <Logos my="4" />
-        <router-view msg="Hello Vue 3 + Element Plus + Vite"/>
-      </div>
-    </div>
+  <el-config-provider namespace="ep" >
+      <div class="common-layout">
+      <el-container style="height: 920px" >
+        <el-header>
+          <BaseHeader />
+        </el-header>
+        <el-container style="height: 100%">
+          
+            <BaseSide />
+
+          <el-main style="height: 100%">
+            <router-view />
+          </el-main>
+        </el-container>
+        
+      </el-container>
+  </div>
+
   </el-config-provider>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+
 
 <style>
 #app {
@@ -17,7 +32,4 @@
   color: var(--ep-text-color-primary);
 }
 
-.main-container {
-  height: calc(100vh - var(--ep-menu-item-height) - 3px);
-}
 </style>
--- IAP/src/router.ts
+++ IAP/src/router.ts
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 import Weather from './components/Weather.vue'
 import Upload1 from './components/utils/UpLoad1.vue'
 import Info from './components/Info.vue'
+import BaseMapVue from './components/map/BaseMap.vue'
 
 
 const routes = [
@@ -10,6 +11,7 @@ const routes = [
     { path: '/weather', component: Weather },
     { path: '/upload1', component: Upload1 },
     { path: '/info', component: Info },
+    { path: '/basemap', component: BaseMapVue },
     ]
   
 export const router = createRouter({

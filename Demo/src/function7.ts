2c22152fab39b6b1e5898bc9c04ae6495ce684a0
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-09-02 17:41:24 +0800

    feat: 计算景点人数

--- Demo/src/function7.ts
+++ Demo/src/function7.ts
@@ -2,6 +2,9 @@ import { GeoFeatures2Arr, GeoPolygons2SimpleArr, readDataFromGeoJSON } from "./A
 import { drawMultiPoint2BLMap, drawSimplePolygon2Map, innerIcon } from "./helpers/BLDraw";
 import { loadBaiDuMap } from "./helpers/initMap";
 import { PointInsidePolygon } from "./packages/CGUtils";
+import { SpherePolygonArea } from "./packages/Distance";
+import { LineString } from "./packages/Geometry";
+import { createPointListFromArr } from "./packages/MetaData";
 
 /**
  * PZQ
@@ -21,17 +24,25 @@ export function function7(
             let interests = simPolygons.slice(1,4); // 0 为总区域
 
             let result = []; // 三个区域的兴趣点
+            let D = []; // 游客密度 人/ km^2
+
             for(let i = 0; i < interests.length ; i++){
                 let temp = [];
+                let count = 0; // 兴趣点个数
                 for(let j = 0 ; j < pois.length ; j++){
                     if(
                         PointInsidePolygon(pois[j],interests[i])
                         ){
                         temp.push(pois[j]);
+                        count++;
                     }
                 }
+                let area = SpherePolygonArea(interests[i]);
+                console.log(area);
+                D.push(count / area);
                 result.push(temp);
             }
+            console.log(D);
 
             result.forEach((item) => {
                 drawMultiPoint2BLMap(item,map);

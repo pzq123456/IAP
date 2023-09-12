999bd2dc3b2b769f9cc86710e808a34c2ad51532
zqy <2398469921@qq.com>
2023-09-12 21:48:48 +0800

    111

--- Demo/src/function3.ts
+++ Demo/src/function3.ts
@@ -27,8 +27,7 @@ export function function3(map: any){
     createBtnList([
         {'name': '加载景点多边形', 'action': () => step1(map)},
         {'name': '加载游客位置', 'action': () => step2(map)},
-        {'name': '渲染多边形并计算游客数量', 'action': () => step3(map)},
-        {'name': '计算游客密度', 'action': () => step4(map)},
+        {'name': '计算景点客流量并展示', 'action': () => step3(map)},
         {'name': '清空地图', 'action': () => map.clearOverlays()}
     ]);
 }
@@ -51,7 +50,7 @@ function step1(map){ //加载数据
 
 function step2(map){
     // load data
-    removeAllOverlay(map);//清空地图
+    
     readDataFromGeoJSON("polygon.json").then((res) => {
         // 向地图上添加景点多边形
         let arr = GeoFeatures2Arr(res.data.features);
@@ -73,7 +72,7 @@ function step2(map){
 }
 
 function step3(map){
-    removeAllOverlay(map);//清空地图
+    
     readDataFromGeoJSON("polygon.json").then((res) => {
         let arr = GeoFeatures2Arr(res.data.features);
         let simPolygons = GeoPolygons2SimpleArr(arr);
@@ -140,50 +139,6 @@ function rapperColor(value:number, values:number[]){
 }
 
 
-function step4(map){
-    removeAllOverlay(map);//清空地图
-    readDataFromGeoJSON("polygon.json").then((res) => {
-        let arr = GeoFeatures2Arr(res.data.features);
-        let simPolygons = GeoPolygons2SimpleArr(arr);
-
-    readDataFromGeoJSON("people1.json").then((res) => {
-        let pois = GeoFeatures2Arr(res.data.features);
-        let icon = innerIcon(5);
-         drawMultiPoint2BLMap(pois,map,icon);
-
-        let interests = simPolygons.slice(1,simPolygons.length);
-
-        let result = []; // 三个区域的兴趣点
-        let D = []; // 游客密度 人/ km^2
-        for(let i = 0; i < interests.length ; i++){
-            let temp = [];
-            let count = 0; // 兴趣点个数
-            for(let j = 0 ; j < pois.length ; j++){
-                if(
-                    PointInsidePolygon(pois[j],interests[i])
-                    ){
-                    temp.push(pois[j]);
-                    count++;
-                    }
-            }
-
-            // let area = SpherePolygonArea(interests[i]);
-            D.push(count);
-            result.push(temp);
-        }
-
-        result.forEach((item) => {
-            drawMultiPoint2BLMap(item,map,innerIcon(6));
-        });
-    
-        interests.forEach((item,index) => {
-            drawSimplePolygon2Map(item,map,rapperColor(D[index],D));
-        })
-        showColorLegend(D,color);
-        addCom2Page(document.querySelector<HTMLDivElement>('#components')!,D,['']);
-        });
-    });
-}
 
 
 function addCom2Page(

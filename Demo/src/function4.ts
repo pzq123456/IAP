d4c085d3b5252e767c577c928167242154e757a7
zqy <2398469921@qq.com>
2023-09-12 16:27:08 +0800

    11

--- Demo/src/function4.ts
+++ Demo/src/function4.ts
@@ -44,7 +44,7 @@ export function function4(map: any){
     createBtnList([
         {'name': '加载数据', 'action': () => step1(map)},
         {'name': '计算狄罗妮三角网（及其中心点）', 'action': () => step2(map)},
-        {'name': '根据三角网追踪voironoi图并裁剪', 'action': () => step3(map)},
+        {'name': '根据三角网追踪Voronoi图并裁剪', 'action': () => step3(map)},
         {'name': '计算到当前点的距离并着色', 'action': () => step4(map)},
         {'name': 'clear', 'action': () => map.clearOverlays()}
     ]);
